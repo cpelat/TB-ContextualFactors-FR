c0fe147b6c60ac674e02bd4f4bbae33f5e4770b5
cpelat <camille.pelat@santepubliquefrance.fr>
2025-08-25 17:22:35 +0200

    Result tables and graphics

--- 03_univariable_models.R
+++ 03_univariable_models.R
@@ -15,10 +15,7 @@ library(INLA)
 library(ggpubr)
 library(flextable)
 
-
-set_flextable_defaults(font.size = 10)
-setEPS() # Option to generate EPS file with the postscript function
-
+set_flextable_defaults(  font.size = 10 )
 
 #--------- Read the useful function bundle ------------
 source( "include/functions.R", encoding = "UTF-8" )
@@ -34,13 +31,7 @@ tb.adj <- readRDS("neighboring_structure_PMSI21.rds" %>% datapath)
 # Create the adjacency matrix
 W <- spdep::nb2mat( neighbours = tb.adj, style = "B", zero.policy = TRUE )
 
-#------ Read shapefiles of French "départements" (districts) and regions -------
-map_dep <- readRDS(datapath("France_shapefiles/map_PMSI21_dep.rds"))  
-map_reg <- readRDS(datapath("France_shapefiles/map_PMSI21_reg.rds"))
 
-
-
-#----------------- Modeling with INLA -----------------------
 # Priors for the BYM2 model
 prior.prec.bym2 <- list(
   prec = list(
@@ -108,7 +99,8 @@ list_bym2_univ_rw2[[v]] <- get_univ_model(
   form_v = '{v}')
 
 
-
+# Save the model list
+saveRDS(list_bym2_univ_rw2, respath("list_bym2_univ_rw2.rds"))
 
 
 
@@ -154,24 +146,201 @@ list_bym2_univ_rw2_inter[[v]] <- get_univ_model(
                                   )',
   form_v ="-1 + period:dens_2010" )  
 
-
-#------------------------------------------------------------------------------#
-#--- Show model results :
-# Fixed effects
-model_bym2_empty$summary.fixed
-
-# Hyperparameters
-model_bym2_empty$summary.hyperpar
-
-# Variance of the spatial random effect
-var_bym2_empty <- model_bym2_empty %>% get_marginal_var_sample() %>% get_marginal_mean()
-var_bym2_empty
-
-# Goddness-of-fit metrics: DIC and WAIC 
-model_bym2_empty$dic$dic
-model_bym2_empty$waic$waic
+# Save the model list
+saveRDS(list_bym2_univ_rw2_inter, respath("list_bym2_univ_rw2_inter.rds"))
 
 
 
 
+
+#-------------- Table of univariable models performance -----------------------
+model_bym2_empty <- readRDS( "model_bym2_empty.rds" %>% respath)
+
+# 1) DIC and WAIC
+list_bym2_univ_rw2_0 <- c(
+  list_bym2_univ_rw2[names(list_bym2_univ_rw2) %in% c(list_var_cont_all, "dens_2010")], 
+  list_bym2_univ_rw2_inter %>% add_suffix_to_name)
+list_bym2_univ_rw2_0[["Empty model"]] <- model_bym2_empty
+res_rw <- get.DIC.table( model.list = list_bym2_univ_rw2_0, digits=2) 
+res_rw
+
+saveRDS(res_rw, "res_rw.rds" %>% respath)
+
+
+# 2) Inter decile standardized rate ratio IdRR (for the models without period interaction)  
+# 2.1) Find the decile values
+q1 <- 0.1
+q2 <- 0.9
+
+list_q <- list()
+for(v in list_var_cont_all){
+  print(v)
+  list_q[[v]] <- map_PMSI21_2[[v]] %>% quantile(probs = c(q1, q2)) 
+}
+
+# For the density, the deciles are the first and last levels :
+map_PMSI21_2[["dens_2010"]] %>% table %>% prop.table
+# sparse intermediate        dense 
+#0.6349837    0.2641850    0.1008312 
+
+list_q[["dens_2010"]] <- map_PMSI21_2[["dens_2010"]] %>% levels %>% extract(c(1,3))
+
+list_q
+
+# IdRR computation  
+res_univ <- NULL
+for(v in c(list_var_cont_all, "dens_2010")){
+  print(v)
+  # Generate a sample of size 100 from the posterior distribution
+  samples <- inla.posterior.sample(100, list_bym2_univ_rw2[[v]], verbose=TRUE )
+  
+  # Calculate the IdRR
+  res_univ <- bind_rows(res_univ,  get_IqRR_stats(v, samples, list_q) )
+}
+
+res_univ %<>%
+  mutate(
+    IdRR = str_glue("{formatC(IqRR_mean, format='f', digits=2, flag='0')} ({IqRR_0.025 %>% formatC(format='f', digits=2, flag='0')}, {IqRR_0.975 %>% formatC(format='f', digits=2, flag='0')})")) 
+
+res_univ
+
+
+#----- stopped here
+# 3) Merger + ajouter le nom des variables
+res_rw2 <- res_rw %>%
+  filter(v0 %in% c(res_univ$variable, "Empty model")) %>% 
+  left_join(res_univ %>% select(variable, IdRR, IqRR_mean), by=c(v0 = "variable")) %>% 
+  mutate(Variable = v0 %>%
+           str_remove_all('_log') %>% 
+           str_remove_all('_g') %>% 
+           get_var_name(nom_type="nom") %>%
+           str_replace_all( fixed("\\n"), " " ),
+         DIC = formatC(DIC, big.mark = ",", format="d")
+  ) %>%
+  arrange(DIC) %>%
+  select(v0, 
+         "Univariable model" = Variable, 
+         DIC,
+         "Inter-decile standardized rate ratio \n(95% Credible Interval)" = IqRR
+  )  
+res_rw2
+
+saveRDS(res_rw2, "res_rw2.rds" %>% respath)
+#res_rw2 <- readRDS("res_rw2.rds" %>% respath)
+
+tt <- qflextable(res_rw2 %>% select(-v0)) 
+tt
+save_as_docx( tt, 
+              values = NULL, 
+              path="IqRR_DIC_model_univ_90.docx"  %>% respath, 
+              pr_section = prop_section(page_size = page_size(orient = "portrait")), 
+              align = "center")
+
+
+
+#------------ Graphique des contributions des variables dans les modeles univariés sans slope aléatoire -----
+listvar <- res_rw2$v0[!(res_rw2$v0 %in% c("dens_2010", "Empty model"))] 
+
+titre <- get_var_name( listvar  %>% 
+                         str_remove_all( "_g" )  %>% 
+                         str_remove_all( "_log" )  ,
+                       nom_type="nom" ) %>% 
+  str_replace_all( fixed("\\n"), " " )
+names( titre ) <- listvar
+
+y_title <- ""
+
+plotlist <- list()
+v <- "dens_2010"
+nomvar = "Population density level"
+plotlist[[v]] <- trace_forestplot(mod=list_bym2_univ_rw2[[ v ]],
+                                  v,
+                                  nomvar=nomvar, 
+                                  y_title=y_title,
+                                  ylim=c(-0.5, 0.6),
+                                  axis.title.size = 14)
+
+for(i in seq_along(listvar)){
+  v <- listvar[i]
+  plotlist[[v]] <- trace_ribbon(tmp=list_bym2_univ_rw2[[ v ]]$summary.random[[v]],
+                                v, 
+                                titre[v], 
+                                y_title=y_title, ylim=c(-0.5, 0.6),
+                                axis.title.size = 14
+  )
+}
+
+# Sorting by increasing DIC
+plotlist <- plotlist[res_rw2$v0[!(res_rw2$v0 %in% c("Empty model"))] ]
+
+ggfinal <- ggarrange(  plotlist = plotlist,
+                       ncol = 2,
+                       nrow = ceiling(length(plotlist)/2), 
+                       labels = "AUTO",
+                       hjust = -1.2
+) %>%
+  annotate_figure(  
+    left = text_grob( "Contribution of each variable to the linear predictor (logarithm of TB standardized notification rates)", rot = 90, size=14), #, just ='top'
+  )
+ggfinal
+
+CairoPNG("var_contrib_bym2_univ.png" %>% respath, width=600, height=800)
+print(  ggfinal )
+dev.off()
+
+postscript("var_contrib_bym2_univ.eps" %>% respath, width=8, height=10)
+print(  ggfinal )
+dev.off()
+
+
+
+
+#------------ Graphique des contributions des variables dans les modeles univariés avec interaction periode * variable -----
+titre <- get_var_name( listvar  %>% 
+                         str_remove_all( "_g" )  %>% 
+                         str_remove_all( "_log" )  ,
+                       nom_type="nom" ) %>% 
+  str_replace_all( fixed("\\n"), " " )
+names( titre ) <- listvar
+
+y_title <- ""
+
+plotlist <- list()
+v <- "dens_2010"
+nomvar = "Population density level"
+plotlist[[v]] <- trace_forestplot_inter(
+  mod=list_bym2_univ_rw2_inter[[v]], 
+  v=v,
+  nomvar=nomvar,  
+  y_title=y_title,
+  axis.title.size = 14)
+
+for(i in seq_along(listvar)){
+  v <- listvar[i]
+  plotlist[[v]] <- trace_ribbon_inter(tmp=list_bym2_univ_rw2_inter[[v]]$summary.random[[v]],
+                                      v=v, 
+                                      nomvar=titre[v],
+                                      y_title=y_title, 
+                                      ylim=c(-0.5, 0.6),
+                                      axis.title.size = 14)
+}
+
+# Sorting by increasing DIC
+plotlist <- plotlist[res_rw2$v0[!(res_rw2$v0 %in% c("Empty model"))] ]
+
+ggfinal <- ggarrange(  plotlist = plotlist,
+                       ncol = 2,
+                       nrow = ceiling(length(plotlist)/2),
+                       labels = "AUTO",
+                       hjust = -1.2
+) %>%
+  annotate_figure(  
+    left = text_grob( "Contribution of each variable to the linear predictor  (logarithm of TB standardized notification rates)", rot = 90, size=14))
+ggfinal
+
+CairoPNG("var_contrib_bym2_univ_inter.png" %>% respath, width=600, height=800)
+print(  ggfinal )
+dev.off()
+
+ggsave("var_contrib_bym2_univ_inter.pdf" %>% respath, plot=ggfinal, width=8, height=10)
 
